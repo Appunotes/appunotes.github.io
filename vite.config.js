ce12cd8a05e9f50aab6c313382ea216f0533d8c9
Aparna Am <aparna63.a@gmail.com>
2026-03-30 14:59:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: 'https://appunotes.github.io/aparna.github.io/',   // 🔴 THIS IS THE FIX
 })
