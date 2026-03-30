3f53664fbc83ad7b5eacaf836eebc5880ffcc7f8
Aparna Am <aparna63.a@gmail.com>
2026-03-30 15:08:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aparna.github.io/',   // 🔴 THIS IS THE FIX
+  base: '/appunotes.github.io/',   // 🔴 THIS IS THE FIX
 })
